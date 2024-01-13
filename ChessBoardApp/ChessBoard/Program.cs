3c5a44c03c88ae4a2965db22ffd953dc874b2776
Galin <galin_3xg@abv.bg>
2024-01-13 21:46:47 +0200

    fix some issues with "try" and "catch"

---
+++ ChessBoardApp/ChessBoard/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using ChessBoardModel;
+
+namespace ChessBoard
+{
+    internal class Program
+    {
+        static Board myBoard = new Board(8);
+        static void Main(string[] args)
+        {
+            // show the empty chess board
+            printBoard(myBoard);
+
+            // ask the user for an x and y coordinate where we will place a piece
+            Cell currentCell = setCurrentCell();
+            currentCell.CurrentlyOccupied = true;
+
+            // calculate all legal moves for that piece
+            myBoard.MarkNextLegalMoves(currentCell, "Knight");
+
+            // print the chess board. Use an "x" for occupied square
+            printBoard(myBoard);
+
+            // wait for another enter key press before ending program.
+            Console.ReadLine();
+        }
+
+        private static Cell setCurrentCell()
+        {
+            int currentRow = 0;
+            int currentCol = 0;
+            try
+            {
+                Console.WriteLine("Enter the current row number");
+                currentRow = int.Parse(Console.ReadLine());
+            }
+            catch 
+            {
+                Console.WriteLine("Not valid. Default set to 3");
+                currentRow = 3;
+            }
+
+            try
+            {
+                Console.WriteLine("Enter the current column number");
+                currentCol = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Not valid. Default set to 3");
+                currentCol = 3;
+            }
+
+            if (currentRow < 0 || currentRow > myBoard.Size || currentCol < 0 || currentCol > myBoard.Size)
+            {
+                Console.WriteLine("out of range. Using 3,3");
+                currentCol = 3;
+                currentRow = 3;
+            }
+            myBoard.theGrid[currentRow, currentCol].CurrentlyOccupied = true;
+
+            return myBoard.theGrid[currentRow, currentCol];
+        }
+
+        private static void printBoard(Board myBoard)
+        {
+            //print the chess board to the console.
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int b = 0; b < myBoard.Size; b++)
+                {
+                    Console.Write("+---");
+                }
+                Console.Write("+");
+                Console.WriteLine();
+                Console.Write("|");
+
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+
+                    Cell c = myBoard.theGrid[i, j];
+
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write(" X ");
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write(" + ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+
+                    }
+                    Console.Write("|");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int d = 0; d < myBoard.Size; d++)
+            {
+                Console.Write("+---");
+            }
+            Console.Write("+");
+            Console.WriteLine();
+
+            Console.WriteLine("===============================");
+        }
+    }
+}
---
+++ ChessBoardApp/ChessBoardModel/ChessBoardModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    public class Board
+    {
+        // Size of board usually 8x8
+        public int Size { get; set; }
+        
+        // 2D array type Cell
+        public Cell[,] theGrid { get; set; }
+
+        //constructor
+        public Board (int s)
+        {
+            //initial size of board
+            Size = s;
+
+            //create a new 2D array of type cell
+            theGrid = new Cell[Size, Size];
+
+            //fill the 2D array with the new Cell, with x and y coordinates
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public bool isSafe(int x, int y)
+        {
+            if (x < 0 || x >= Size || y < 0 || y >= Size)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void MarkNextLegalMoves (Cell currentCell, string chessPiece)
+        {
+            // clear all previous legal move
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+            // find all legal moves and mark cells as "legal"
+
+            switch (chessPiece)
+            {
+                case "Knight":
+                    if (isSafe(currentCell.RowNumber + 2, currentCell.ColNumber + 1))
+                    {
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColNumber + 1].LegalNextMove = true;
+                    }
+                    if (isSafe(currentCell.RowNumber + 2, currentCell.ColNumber - 1))
+                    {
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColNumber - 1].LegalNextMove = true;
+                    }
+                    if (isSafe(currentCell.RowNumber - 2, currentCell.ColNumber + 1))
+                    {
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColNumber + 1].LegalNextMove = true;
+                    }
+                    if (isSafe(currentCell.RowNumber - 2, currentCell.ColNumber - 1))
+                    {
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColNumber - 1].LegalNextMove = true;
+                    }
+                    if (isSafe(currentCell.RowNumber + 1, currentCell.ColNumber + 2))
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColNumber + 2].LegalNextMove = true;
+                    }
+                    if (isSafe(currentCell.RowNumber + 1, currentCell.ColNumber - 2))
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColNumber - 2].LegalNextMove = true;
+                    }
+                    if (isSafe(currentCell.RowNumber - 1, currentCell.ColNumber + 2))
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColNumber + 2].LegalNextMove = true;
+                    }
+                    if (isSafe(currentCell.RowNumber - 1, currentCell.ColNumber - 2))
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColNumber - 2].LegalNextMove = true;
+                    }
+
+                    break;
+
+                case "King":
+                    break;
+
+                case "Rook":
+                    break;
+
+                case "Bishop":
+                    break;
+
+                case "Queen":
+                    break;
+
+                default:
+                    break;
+            }
+            theGrid[currentCell.RowNumber, currentCell.ColNumber].CurrentlyOccupied = true;
+        }
+    }
+}
